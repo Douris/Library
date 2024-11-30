040814a2e8acbc897470495e659b5d02a722a99b
Mamadou Oury <ouryboulloye@gmail.com>
2024-11-30 20:49:27 +0100

    initial commit

---
+++ library/src/test/java/com/baldeoury/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baldeoury.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
